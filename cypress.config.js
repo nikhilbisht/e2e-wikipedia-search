9172f03408e4f631df6fe3b17001098db9179b0f
Nikhil Bisht <Nbisht@republicfinance.com>
2022-07-23 15:55:30 +0530

    added command to generate html report and added dependencies for report generation

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight : 1920,
   viewportHeight : 1080,
+  video : false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl : "https://www.wikipedia.org",
     setupNodeEvents(on, config) {
