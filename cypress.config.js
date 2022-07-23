1da20f4fe4bdd1e081ae9c4dae2117dbdff4df88
Nikhil Bisht <Nbisht@republicfinance.com>
2022-07-23 13:22:22 +0530

    added baseUrl, viewPortWidth and viewPortHeight

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight : 1920,
+  viewportHeight : 1080,
   e2e: {
+    baseUrl : "https://www.wikipedia.org",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
